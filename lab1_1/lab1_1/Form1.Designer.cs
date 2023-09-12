723342a4ac89219dffeefb2f0aaab9fe329f9bc7
dmytro1312 <144057721+dmytro1312@users.noreply.github.com>
2023-09-12 21:45:06 +0300

    Add files via upload

---
+++ lab1_1/lab1_1/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace lab1_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, ((System.Drawing.FontStyle)((((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline) 
+                | System.Drawing.FontStyle.Strikeout))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(54, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(405, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Година за годиною — та й так вік зійде.";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(273, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Історія №1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.DarkGreen;
+            this.button2.Location = new System.Drawing.Point(114, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(273, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Історія №2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(114, 154);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(273, 38);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Історія №3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(114, 198);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(273, 36);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Закрити програму";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(502, 76);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(265, 78);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Відкрити форму";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::lab1_1.Properties.Resources.f84d217853d263e771f2d4ffc4c6fcef_XL;
+            this.pictureBox1.Location = new System.Drawing.Point(433, 198);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(410, 394);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(855, 537);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Вас вітає студент групи ІПЗ-3/1 Дмитрук Дмитро Олександрович";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ lab1_1/lab1_1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Розпочинаємо вивчення основ ООП", "ООП", MessageBoxButtons.YesNo, MessageBoxIcon.Stop);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Йде собі їжачок лісом. Бачить - стоїть їжачка раком. Ну, їжачок і так до неї, і сяк - не дістає. Побіг, приніс цеглу – знову не дістає. Збігав ще за одним, а їжачка вже пішла. Їжачок: - Не дуже й хотілося...", "Анекдот",MessageBoxButtons.OK,MessageBoxIcon.Question );
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            (new Form1()).Show();   
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("В інкубаторі, сидів дідок.\r\nПочухав яйця і сказав:\r\n\"Ти хочешь в армію? Во-о-во!\r\nТи, підеш в армію!\" Є-є-а.\r\n \r\nА за горою, пасся петух.\r\nКорова перднула, петух потух.\r\nНе хочу в армію! Во-о-во!\r\nНе піду в армію. Є-є-а.\r\n \r\nЯ пам'ятаю, свое рідне село,\r\nЯк я на тракторі ганяв у кіно.\r\nНе хочу в армію! Во-о-во!\r\nНе піду в армію. Є-є-а.\r\n \r\nЯ пам'ятаю, свое рідне село,\r\nЯк я лопатою черпав ґівно.\r\nНе хочу в армію! Во-о-во!\r\nНе піду в армію. Є-є-а.\r\n \r\nЯ пам'ятаю рідний мопед,\r\nЯк на ньому я зламав хребет.\r\nНе хочу в армію! Во-о-во!\r\nНе піду в армію. Є-є-а.\r\n \r\nА я назбираю мішок гранат,\r\nІ підірву военкомат!\r\nІ підірву його! Во-о-во!\r\nІ підірву його! Є-є-а.\r\n \r\nА в армії є йще один военкомат,\r\nА я назбіраю йще один мішок гранат.\r\nІ підірву його! Во-о-во!\r\nІ підірву його! Є-є-а.", "Не піду в армію", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+        }
+    }
+}
